c9d36a3cbc7727cd6e5cacc616835210eb5eb485
Hana Acciaioli <hana.acciaioli@gmail.com>
2022-11-01 14:22:02 -0700

    test created for zodiac/:id route

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,12 +3,23 @@ const app = require('../lib/app');
 
 const { signs } = require('../lib/signs-data.js');
 
-describe('signs routes', () => {
-  it('/signs should return a list of zodiac signs with id', async () => {
+describe('zodiac routes', () => {
+  it('/zodiac should return a list of zodiac signs with id', async () => {
     const res = await request(app).get('/zodiac');
     const expected = signs.map((sign) => {
       return { id: sign.id, name: sign.name };
     });
     expect(res.body).toEqual(expected);
+  });
+
+  it('/zodiac/:id should return zodiac sign detail', async () => {
+    const res = await request(app).get('/zodiac/1');
+    const aquarius = {
+      id: '1',
+      name: 'aquarius',
+      dates: 'Jan 21 - Feb 19',
+      symbol: 'Water Bearer',
+    };
+    expect(res.body).toEqual(aquarius);
   });
 });
