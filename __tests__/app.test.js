9dbd366a1e6ecfc25d4205c60b541ff3a4fb7bbf
Hana Acciaioli <hana.acciaioli@gmail.com>
2022-11-01 14:13:14 -0700

    zodiac route tested and passed

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -5,7 +5,7 @@ const { signs } = require('../lib/signs-data.js');
 
 describe('signs routes', () => {
   it('/signs should return a list of zodiac signs with id', async () => {
-    const res = await request(app).get('/signs');
+    const res = await request(app).get('/zodiac');
     const expected = signs.map((sign) => {
       return { id: sign.id, name: sign.name };
     });
